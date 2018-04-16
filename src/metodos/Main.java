64daa66104d62a9ac90f62eb1f771bfc800eef8a
Luis Doratt <luisdoratt@hotmail.com>
2018-04-15 21:55:00 -0600

    Bairstow

--- src/metodos/Main.java
+++ src/metodos/Main.java
@@ -5,6 +5,8 @@
  */
 package metodos;
 
+import java.util.Arrays;
+
 
 
 /**
@@ -15,13 +17,7 @@ public class Main {
     
     public static void main(String[] args) {
         Bairstow bair = new Bairstow();
-        double[] bs, cs;
-        
-        bs= (bair.calcularValores(3, new double[]{1,5,7,3}, -2, -2));
-        cs = bair.calcularValores(3, bs, -2, -2);
-        System.out.println(Arrays.toString(bs));
-        System.out.println(Arrays.toString(cs));
-        bair.bairstow(bs[bs.length-1], bs[bs.length-2], cs[cs.length-2], cs[1], cs[0]);
+        bair.bairstow(-2, -2, new double[]{1, 5, 7, 3}, 3);
     }
     
     
